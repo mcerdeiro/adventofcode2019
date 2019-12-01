177921f24af213da7ab4b89ec0722ad72df0f9d8
Mariano Cerdeiro <mcerdeiro@gmail.com>
2019-12-01 07:02:39 +0100

    solution day01

--- day01/day01.py
+++ day01/day01.py
@@ -1,9 +1,29 @@
+#!/usr/bin/python3
+
+def fuel(fuelm): 
+    fuelt = 0   
+    fuel = int(int(fuelm) / 3) - 2
+    while(fuel > 0):
+        print("Mass: " + str(fuelm) + " Requires: " + str(fuel))
+        fuelt += fuel
+        fuelm = fuel
+        fuel = int(int(fuelm) / 3) - 2
+        print("New "+ str(acount))
+    
+    return fuelt
+
 file = open("day01.dat", "r")
-lines = file.readlines()
+lines = file.read().splitlines()
 file.close()
 
+acount = 0
+bcount = 0
+array = []
 for line in lines:
-    line = line.rstrip()
-    print("Line: " + line)
-    for c in line:
-        print("C: " + c)
+    val = int(int(line) / 3) - 2
+    print("Line: " + line + "- Val: " + str(val))
+    acount = acount + val
+    #print("Input: "+ str(val) + " Fuel: " + str(fuel(val)))
+    acount += fuel(val)
+
+print("part02: " +  str(acount))
